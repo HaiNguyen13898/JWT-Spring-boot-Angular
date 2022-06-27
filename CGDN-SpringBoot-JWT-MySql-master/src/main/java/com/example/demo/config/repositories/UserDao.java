bd27275cde23876ad1919b13926d98c2c1f9b936
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-06-27 10:45:58 +0700

    nop bai

---
+++ CGDN-SpringBoot-JWT-MySql-master/src/main/java/com/example/demo/config/repositories/UserDao.java
@@ -0,0 +1,12 @@
+package com.example.demo.config.repositories;
+
+import com.example.demo.config.entity.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+
+    DAOUser findByUsername(String username);
+}
